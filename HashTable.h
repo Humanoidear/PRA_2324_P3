d8dec092355880fc7eba74ad77a601ed789ac091
Alex <basurillodesgastorito@gmail.com>
2024-11-12 20:05:40 +0100

    Añadir

--- HashTable.h
+++ HashTable.h
@@ -3,10 +3,9 @@
 
 #include <ostream>
 #include <stdexcept>
+#include <list>
 #include "Dict.h"
 #include "TableEntry.h"
-
-#include "/Users/alejandrogonzalez/Desktop/PRA_2324_P1/ListLinked.h"
 
 template <typename V>
 class HashTable: public Dict<V> {
@@ -14,7 +13,7 @@ class HashTable: public Dict<V> {
     private:
         int n;
         int max;
-        ListLinked<TableEntry<V> >* table;
+        std::list<TableEntry<V>>* table;
 
         int h(std::string key);
 
@@ -24,6 +23,12 @@ class HashTable: public Dict<V> {
         int capacity();
         friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th);
         V operator[](std::string key);
+
+        // Implementación de métodos virtuales de Dict
+        void insert(std::string key, V value) override;
+        V search(std::string key) override;
+        void remove(std::string key) override;
+        int entries() override;
 };
 
 
@@ -31,7 +36,7 @@ template <typename V>
 HashTable<V>::HashTable(int size) {
     this->n = 0;
     this->max = size;
-    this->table = new ListLinked<TableEntry<V> >[size];
+    this->table = new std::list<TableEntry<V>>[size];
 }
 
 template <typename V>
@@ -56,7 +61,11 @@ int HashTable<V>::h(std::string key) {
 template <typename V>
 std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
     for (int i = 0; i < th.max; i++) {
-        out << i << ": " << th.table[i] << std::endl;
+        out << i << ": ";
+        for (const auto& entry : th.table[i]) {
+            out << entry << " ";
+        }
+        out << std::endl;
     }
     return out;
 }
@@ -65,11 +74,52 @@ template <typename V>
 V HashTable<V>::operator[](std::string key) {
     int pos = this->h(key);
     TableEntry<V> te(key);
-    if (this->table[pos].search(te) == nullptr) {
-        throw std::invalid_argument("Key not found");
+    for (const auto& entry : this->table[pos]) {
+        if (entry == te) {
+            return entry.getValue();
+        }
     }
-    return this->table[pos].search(te)->value;
+    throw std::invalid_argument("Key not found");
 }
 
+template <typename V>
+void HashTable<V>::insert(std::string key, V value) {
+    int pos = this->h(key);
+    TableEntry<V> te(key, value);
+    this->table[pos].push_back(te);
+    this->n++;
+}
+
+template <typename V>
+V HashTable<V>::search(std::string key) {
+    int pos = this->h(key);
+    TableEntry<V> te(key);
+    for (const auto& entry : this->table[pos]) {
+        if (entry == te) {
+            return entry.getValue();
+        }
+    }
+    throw std::invalid_argument("Key not found");
+}
+
+template <typename V>
+void HashTable<V>::remove(std::string key) {
+    int pos = this->h(key);
+    TableEntry<V> te(key);
+    auto& entries = this->table[pos];
+    for (auto it = entries.begin(); it != entries.end(); ++it) {
+        if (*it == te) {
+            entries.erase(it);
+            this->n--;
+            return;
+        }
+    }
+    throw std::invalid_argument("Key not found");
+}
+
+template <typename V>
+int HashTable<V>::entries() {
+    return this->n;
+}
 
 #endif
