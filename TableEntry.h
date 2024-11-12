06cd7655242f4e3429bfc36a2c0c22e8c0874d99
Alex <basurillodesgastorito@gmail.com>
2024-11-12 20:08:42 +0100

    Añadir

--- TableEntry.h
+++ TableEntry.h
@@ -4,48 +4,63 @@
 #include <string>
 #include <ostream>
 
-template <typename V> 
-class TableEntry {
-    public:
-        std::string key;
-        V value;
+template <typename V>
+class TableEntry
+{
+public:
+    std::string key;
+    V value;
 
-        TableEntry(std::string key, V value);
-        TableEntry(std::string key);
-        TableEntry();
-        friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2);
-        friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2);
-        friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te);
+    TableEntry(std::string key, V value);
+    TableEntry(std::string key);
+    TableEntry();
+    
+    V getValue() const;
+
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2);
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2);
+    friend std::ostream &operator<<(std::ostream &out, const TableEntry<V> &te);
 };
 
-template <typename V> 
-TableEntry<V>::TableEntry(std::string key, V value) {
+template <typename V>
+TableEntry<V>::TableEntry(std::string key, V value)
+{
     this->key = key;
     this->value = value;
 }
 
 template <typename V>
-TableEntry<V>::TableEntry(std::string key) {
+TableEntry<V>::TableEntry(std::string key)
+{
     this->key = key;
 }
 
 template <typename V>
-TableEntry<V>::TableEntry() {
+TableEntry<V>::TableEntry()
+{
     this->key = "";
 }
 
 template <typename V>
-bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+V TableEntry<V>::getValue() const {
+    return value;
+}
+
+template <typename V>
+bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2)
+{
     return te1.key == te2.key;
 }
 
 template <typename V>
-bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2)
+{
     return te1.key != te2.key;
 }
 
 template <typename V>
-std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+std::ostream &operator<<(std::ostream &out, const TableEntry<V> &te)
+{
     out << te.key << " " << te.value;
     return out;
 }
